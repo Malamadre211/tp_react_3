e823692feb5356164a6b64b5c6129fc01c056e04
malamadre211 <mariangelacurcioartist@gmail.com>
2023-11-06 14:08:50 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { useCallback, useState } from 'react';
+import {Dice} from './dice.tsx';
+
+function App() {
+  const [value, setValue] = useState (0);
+  
+  const handleRoll = useCallback(
+    (newValue: number) => {
+      setValue(newValue)
+    }, 
+    [])
+
+return (
+  <>
+  <Dice onRoll={handleRoll} />
+  <p>{value}</p>
+  </>
+)
+}
+
+export default App;
---
+++ src/dice.tsx
@@ -0,0 +1,20 @@
+import { useCallback, useEffect, useState } from 'react';
+
+export function Dice(props: { onRoll: (value: number) => void }) {
+    const [value, setValue] = useState(Math.floor(Math.random()*6+1))
+
+    useEffect(() => {
+        props.onRoll(value)
+    }, [])
+
+    const roll= useCallback(() => {
+        const newValue = Math.floor(Math.random()*6+1)
+        props.onRoll(newValue)
+        setValue(newValue)
+    }, [])
+
+    return(
+    
+        <div onClick={roll}>{value}</div>
+    ) 
+}
